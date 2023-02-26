5858919f4d78ea8bd4b33a27317d4ae6ba1809f0
StephenSQ <stevestove20@gmail.com>
2023-02-26 07:08:05 +0700

    Implemented New Player Movement

--- Entities/Player.gd
+++ Entities/Player.gd
@@ -1,48 +1,44 @@
-extends Area2D
+extends RigidBody2D
 
-export var player_level := 1 # max = 20 
-export var movement_speed := 150 # max = 500
-export var angular_speed := 5 # max = 10
-export var health := 100
-var turn = 0 # for backward movement
+var player_level := 1 # max = 20 
+var movement_speed := 100 # max = 200
+var turning_speed := 500 # max = 800
+var health := 100 # max = 200
+
+export var turn_timing = 0.0 # to control turning in sync with AnimationPlayer
+export var move_timing = 0.0 # to control movement in sync with AnimationPlayer
 var screen_size
 
 func _ready():
 	screen_size = get_viewport_rect().size
 
 func _process(delta):
-	# TURNING MOVEMENT (LEFT RIGHT)
+	# TURNING MOVEMENT
 	var direction = 0
 	if Input.is_action_pressed("move_right"):
 		direction -= 1
 	if Input.is_action_pressed("move_left"):
 		direction += 1
-	rotation += angular_speed * direction * delta
 	
-	# FORWARD BACKWARD MOVEMENT (BACKWARD MOVEMENT IN THE FORM OF TURNING 180 QUICKLY)
+	# FORWARD BACKWARD MOVEMENT
 	var velocity := Vector2.ZERO
 	if Input.is_action_pressed("move_up"):
 		velocity = Vector2.UP.rotated(rotation) * movement_speed
-	if Input.is_action_pressed("move_down"):
-		if turn < PI: # ONLY turn if player hasn't turned for 180 degrees
-			direction = angular_speed * 2 * delta # reused direction variable so that it will still trigger animation 
-			rotation += direction
-			turn += direction
-	if Input.is_action_just_released("move_down"):
-		turn = 0 # reset turn after down is released so the player can turn 180 again
-	position += velocity * delta #HOW DO I IMPLEMENT A BOBBING MOVEMENT HERE
+		apply_central_impulse(velocity * move_timing * delta)
+		apply_torque_impulse(direction * turning_speed * move_timing * delta)
+	else:
+		apply_torque_impulse(direction * turning_speed * turn_timing * delta)
+	
+	
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
 	
 	if velocity:
-		$AnimationPlayer.play("move")
+		$AnimationPlayer.play("move_forward")
 	else:
-		if direction > 0:
-			$AnimationPlayer.play("turn")
-		elif direction < 0:
-			$BodySprite.flip_h = true
+		if direction != 0:
+			$BodySprite.flip_h = direction > 0
 			$AnimationPlayer.play("turn")
 		else:
-			$BodySprite.flip_h = false
 			$AnimationPlayer.stop()
 			$BodySprite.frame = 0
